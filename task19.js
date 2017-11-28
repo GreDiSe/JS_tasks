85f2fcb53d5f3ceec8d5bf8ca89cfcf8a226c6fd
Дима <kykysua@list.ru>
2017-11-28 15:28:57 +0200

    15,17,19 task

--- task19.js
+++ task19.js
@@ -2,6 +2,6 @@ function check(value) {
     return value >= 80 && value <= 120 || value >= 380 && value <= 420
 }
 console.log(check(5)); // false
-console.log(check(50)); // true
+console.log(check(50)); // false
 console.log(check(124)); // false
 console.log(check(400)); // true
