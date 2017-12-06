08dc58ba9fd8ad821b1248ec25e2dc8c6be8e3fd
Дима <kykysua@list.ru>
2017-12-06 14:00:52 +0200

    myPromise

--- task4.js
+++ task4.js
@@ -1,40 +1,40 @@
-var information;
-var status;
+let value;
+let status;
+let ResolveCallBack = [];
+let RejectCallBack = [];
 function MyPromise(fn) {
+    ResolveCallBack = [];
+    RejectCallBack = [];
+    value = undefined;
     status = 'pending';
-    doResolve(fn, onResolve, onReject);
+    fn(inf =>  ResolveCallBack.push(onResolve(inf)), er => RejectCallBack.push(onReject(er)));
 }
+
 function onResolve(inf) {
     status = 'resolved';
-    //console.log('inf:' + inf)
-    information = inf;
-    //console.log('Info: ' + information)
-    //console.log(inf);
+    value = inf;
+    ResolveCallBack.forEach(handler);
 }
-function onReject(error) {
-    //console.log(error);
-    //console.log('error:' + error)
-    information = error;
+function onReject(er) {
     status = 'rejected';
+    value = er;
+    RejectCallBack.forEach(handler);
+}
+function handler(current) {
+    if(!current) return;
+    if(status === 'pending') ResolveCallBack.push(current);
+    else current(value);
+}
 
-}
-function doResolve(fn, resolve, reject) {
-    fn(inf => resolve(inf), res => reject(res));
-}
-function done(resolve, reject) {
-    console.log('_' + status);
-    resolve(information);
 
-}
-MyPromise.prototype.then = function (onResolve, onReject) {
-    return new MyPromise(function (resolve, reject) {
-        return done(function (result) {
-                resolve(onResolve(result))
-            }, function (error) {
-                reject(onReject(error))
-            }
-        );
-    });
+MyPromise.prototype.then = function (resolve, reject) {
+    if(resolve) ResolveCallBack.push(resolve);
+    if(reject) RejectCallBack.push(reject);
+
+    return this;
+};
+MyPromise.prototype.catch = function (reject) {
+    if(reject) RejectCallBack.push(reject);
 };
 function foo() {
     return new MyPromise(function(resolve, reject){
@@ -50,12 +50,11 @@ let p2 = foo();
 let p3 = foo();
 
 p1
-    .then(info => console.info(info));
-    // .catch(er => console.error(er));
-p2
-    .then(info => console.info(info));
-//     .catch(er => console.error(er));
-p3
-    .then(info => console.info(info));
-//     .catch(er => console.error(er));
-
+    .then(info => console.info('Out: ' + info))
+    .catch(er => console.error('Er: ' + er))
+// p2
+//     .then(info => console.info(info))
+//     .catch(er => console.error(er))
+// p3
+//     .then(info => console.info(info))
+//     .catch(er => console.error(er))
