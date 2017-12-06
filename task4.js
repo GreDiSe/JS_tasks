e6e43d2d61d56c260111235c1c1130004c4fe9e9
Дима <kykysua@list.ru>
2017-12-06 11:16:14 +0200

    myPromise

--- task4.js
+++ task4.js
@@ -1,5 +1,43 @@
+var information;
+var status;
+function MyPromise(fn) {
+    status = 'pending';
+    doResolve(fn, onResolve, onReject);
+}
+function onResolve(inf) {
+    status = 'resolved';
+    //console.log('inf:' + inf)
+    information = inf;
+    //console.log('Info: ' + information)
+    //console.log(inf);
+}
+function onReject(error) {
+    //console.log(error);
+    //console.log('error:' + error)
+    information = error;
+    status = 'rejected';
+
+}
+function doResolve(fn, resolve, reject) {
+    fn(inf => resolve(inf), res => reject(res));
+}
+function done(resolve, reject) {
+    console.log('_' + status);
+    resolve(information);
+
+}
+MyPromise.prototype.then = function (onResolve, onReject) {
+    return new MyPromise(function (resolve, reject) {
+        return done(function (result) {
+                resolve(onResolve(result))
+            }, function (error) {
+                reject(onReject(error))
+            }
+        );
+    });
+};
 function foo() {
-    return new Promise(function(resolve, reject){
+    return new MyPromise(function(resolve, reject){
         setTimeout(() => {
             if(Math.random() > 0.5) resolve('Правда');
             else reject('Неправда');
@@ -12,12 +50,12 @@ let p2 = foo();
 let p3 = foo();
 
 p1
-    .then(info => console.info(info))
-    .catch(er => console.error(er));
+    .then(info => console.info(info));
+    // .catch(er => console.error(er));
 p2
-    .then(info => console.info(info))
-    .catch(er => console.error(er));
+    .then(info => console.info(info));
+//     .catch(er => console.error(er));
 p3
-    .then(info => console.info(info))
-    .catch(er => console.error(er));
+    .then(info => console.info(info));
+//     .catch(er => console.error(er));
 
