543e228ce7e88b2afa417b970bca0028336d13f0
Дима <kykysua@list.ru>
2017-12-15 12:04:18 +0200

    MVC

--- scripts/View/table.js
+++ scripts/View/table.js
@@ -5,25 +5,23 @@ class Table {
         this.mine = mine;
         this.container = document.getElementById('container')
     }
-}
+    createTable() {
 
-Table.prototype.createTable = function () {
+        this.container.style.width = this.col * 28 + 'px';
+        this.container.style.margin = '0 auto';
+        this.container.style.border = '1px solid black';
 
-    this.container.style.width = this.col * 28 + 'px';
-    this.container.style.margin = '0 auto';
-    this.container.style.border = '1px solid black';
-
-    for(let i = 0; i < this.row; i++){
-        let tr = document.createElement('tr');
-        this.container.appendChild(tr);
-        for(let j = 0; j < this.col; j++){
-            let td = document.createElement('td');
-            td.className = 'td';
-            tr.appendChild(td);
+        for(let i = 0; i < this.row; i++){
+            let tr = document.createElement('tr');
+            this.container.appendChild(tr);
+            for(let j = 0; j < this.col; j++){
+                let td = document.createElement('td');
+                td.className = 'td';
+                tr.appendChild(td);
+            }
         }
-    }
-};
-
-Table.prototype.removeTable = function () {
-    this.container.innerHTML = '';
-};
+    };
+    removeTable() {
+        this.container.innerHTML = '';
+    };
+}
